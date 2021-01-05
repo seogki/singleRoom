226e42c3242f0e2d1905478c47b78be76e6e8695
seogkiHong <skhong@eco.kr>
2021-01-05 16:32:47 +0900

    test

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="home-component">
+    홈테스트
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home-component",
+};
+</script>
+
+<style></style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,1 +1,14 @@
-module.exports = { outputDir: "../src/main/resources/static", indexPath: "../static/index.html" };
+module.exports = {
+  outputDir: "../src/main/resources/static",
+  indexPath: "../static/index.html",
+
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8900",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+};
