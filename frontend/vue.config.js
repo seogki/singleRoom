a6d0c2e156f1e017cee3c4eaa5109fbe0b34df7c
seogkiHong <skhong@eco.kr>
2021-01-05 15:44:52 +0900

    first commit

---
+++ frontend/vue.config.js
@@ -0,0 +1,1 @@
+module.exports = { outputDir: "../src/main/resources/static", indexPath: "../static/index.html" };
---
+++ src/test/java/com/skh/sroom/SroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skh.sroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
