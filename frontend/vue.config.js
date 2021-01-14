102efbceaa4b6aeebf594657beeb0a1c383ec92d
seogkiHong <skhong@eco.kr>
2021-01-14 18:04:12 +0900

    vuetify 추가

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-  outputDir: "../src/main/resources/static",
-  indexPath: "../static/index.html",
-
-  devServer: {
-    proxy: {
+  "outputDir": "../src/main/resources/static",
+  "indexPath": "../static/index.html",
+  "devServer": {
+    "proxy": {
       "/api": {
-        target: "http://localhost:8900",
-        ws: true,
-        changeOrigin: true,
-      },
-    },
-  },
-
-  pluginOptions: {
-    apollo: {
-      enableMocks: true,
-      enableEngine: true
+        "target": "http://localhost:8900",
+        "ws": true,
+        "changeOrigin": true
+      }
     }
-  }
-};
+  },
+  "pluginOptions": {
+    "apollo": {
+      "enableMocks": true,
+      "enableEngine": true
+    }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
